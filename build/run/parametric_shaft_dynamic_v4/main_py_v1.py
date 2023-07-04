33b02905b541e1cea80b5980f046c7ab3dab362f
zaxosoik <zaxosoik@gmail.com>
2023-07-04 22:11:41 +0300

    new file:   build/run/parametric_shaft_dynamic_v4/batch_parameters.xml 	modified:   build/run/parametric_shaft_dynamic_v4/main_py_v1.py 	modified:   build/run/parametric_shaft_dynamic_v4/parameters.xml 	modified:   build/run/parametric_shaft_dynamic_v4/test_1/boundary_id.vtu

--- build/run/parametric_shaft_dynamic_v4/main_py_v1.py
+++ build/run/parametric_shaft_dynamic_v4/main_py_v1.py
@@ -15,9 +15,13 @@ class Component:
         self.name = name
         self.xml_element = xml_element
         self.parameters = []
+        self.subcomponents = []
 
     def add_parameter(self, parameter):
         self.parameters.append(parameter)
+
+    def add_subcomponent(self, subcomponent):
+        self.subcomponents.append(subcomponent)
 
 class XMLHandler:
     def __init__(self, xml_data):
@@ -28,16 +32,35 @@ class XMLHandler:
     def parse(self):
         for component in self.root:
             comp = Component(component.tag, component)
-            for parameter in component:
-                name = parameter.tag
-                value = parameter.find('value').text
-                default_value = parameter.find('default_value').text
-                documentation = parameter.find('documentation').text
-                pattern = parameter.find('pattern').text
-                pattern_description = parameter.find('pattern_description').text
-                param = Parameter(name, value, default_value, documentation, pattern, pattern_description, parameter)
-                comp.add_parameter(param)
+            self.parse_parameters(comp, component)
+            self.parse_subcomponents(comp, component)
             self.components.append(comp)
+
+    def parse_parameters(self, parent_component, parent_element):
+        for parameter in parent_element:
+            if parameter.tag.startswith('value'):
+                continue  # Skip the subcomponents section
+            name = parameter.tag
+            value_element = parameter.find('value')
+            value = value_element.text if value_element is not None else None
+            default_value_element = parameter.find('default_value')
+            default_value = default_value_element.text if default_value_element is not None else None
+            documentation_element = parameter.find('documentation')
+            documentation = documentation_element.text if documentation_element is not None else None
+            pattern_element = parameter.find('pattern')
+            pattern = pattern_element.text if pattern_element is not None else None
+            pattern_description_element = parameter.find('pattern_description')
+            pattern_description = pattern_description_element.text if pattern_description_element is not None else None
+            param = Parameter(name, value, default_value, documentation, pattern, pattern_description, parameter)
+            parent_component.add_parameter(param)
+
+    def parse_subcomponents(self, parent_component, parent_element):
+        for subcomponent in parent_element:
+            if not subcomponent.tag.startswith('value'):
+                comp = Component(subcomponent.tag, subcomponent)
+                self.parse_parameters(comp, subcomponent)
+                self.parse_subcomponents(comp, subcomponent)
+                parent_component.add_subcomponent(comp)
 
     def save(self, file_name):
         self.tree.write(file_name)
@@ -47,19 +70,28 @@ class ParameterManager:
         self.handler = handler
 
     def set_parameter_value(self, component_name, parameter_name, new_value):
-        # Iterate over all components
         for component in self.handler.components:
             if component.name == component_name:
-                # If the component is found, iterate over all parameters
-                for parameter in component.parameters:
-                    if parameter.name == parameter_name:
-                        # If the parameter is found, update its value
-                        parameter.value = new_value
-                        parameter.xml_element.find('value').text = new_value
+                if self.update_parameter_value(component, parameter_name, new_value):
+                    return True
+                for subcomponent in component.subcomponents:
+                    if self.update_parameter_value(subcomponent, parameter_name, new_value):
                         return True
         return False
 
-with open('parameters.xml', 'r') as f:
+    def update_parameter_value(self, component, parameter_name, new_value):
+        for parameter in component.parameters:
+            if parameter.name == parameter_name:
+                parameter.value = new_value
+                if parameter.xml_element is not None:
+                    value_element = parameter.xml_element.find('value')
+                    if value_element is not None:
+                        value_element.text = str(new_value)
+                    return True
+        return False
+
+
+with open('batch_parameters.xml', 'r') as f:
     xml_data = f.read()
 
 handler = XMLHandler(xml_data)
@@ -68,9 +100,9 @@ handler.parse()
 manager = ParameterManager(handler)
 
 # Change the value of a specific parameter
-if manager.set_parameter_value('Geometry', 'Half_20length', '15.'):
+if manager.set_parameter_value('Geometry', 'Half_20length_20Last', '20.'):
     print('Parameter value updated successfully.')
 else:
     print('Failed to update parameter value.')
 
-handler.save('parameters.xml')  # save the modified XML tree back to the file
+handler.save('batch_parameters.xml')  # save the modified XML tree back to the file
